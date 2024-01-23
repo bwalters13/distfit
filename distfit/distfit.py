df1f2a90800247fe616779bac84de71507150379
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2024-01-22 16:57:39 -0800

    Update distfit.py

--- distfit/distfit.py
+++ distfit/distfit.py
@@ -1485,7 +1485,7 @@ class distfit:
 
         elif distr=='full':
             # st.levy_l, st.levy_stable, st.frechet_r, st.frechet_l
-            out_distr = [st.alpha, st.anglit, st.arcsine, st.beta, st.betaprime, st.bradford, st.burr, st.cauchy,
+            out_distr = [st.alpha, st.anglit, st.arcsine, st.beta, st.betaprime, st.bradford, st.cauchy,
                          st.chi, st.chi2, st.cosine, st.dgamma, st.dweibull, st.erlang, st.expon, st.exponnorm,
                          st.exponweib, st.exponpow, st.f, st.fatiguelife, st.fisk, st.foldcauchy, st.foldnorm,
                          st.genlogistic, st.genpareto, st.gennorm, st.genexpon, st.genextreme, st.gausshyper, st.gamma,
@@ -1494,7 +1494,7 @@ class distfit:
                          st.invgauss, st.invweibull, st.johnsonsb, st.johnsonsu, st.laplace, st.levy,
                          st.logistic, st.loggamma, st.loglaplace, st.lognorm, st.lomax, st.maxwell, st.mielke,
                          st.nakagami, st.norm, st.pareto, st.pearson3, st.powerlaw, st.powerlognorm, st.powernorm,
-                         st.rdist, st.reciprocal, st.rayleigh, st.rice, st.recipinvgauss, st.semicircular, st.skewnorm, st.t,
+                         st.rdist, st.reciprocal, st.rayleigh, st.rice, st.recipinvgauss, st.semicircular, st.skewcauchy, st.skewnorm, st.t,
                          st.triang, st.truncexpon, st.truncnorm, st.tukeylambda, st.uniform, st.vonmises,
                          st.vonmises_line, st.wald, st.weibull_min, st.weibull_max, st.wrapcauchy]
 
